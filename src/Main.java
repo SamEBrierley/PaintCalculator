e5adba1a438d316d75f8dae763d0519068e0cb95
Sam Ellis Brierley <samellisbrierley@gmail.com>
2022-01-20 16:39:04 +0000

    Added a feature to state whether a ladder is required for tall walls and calculates whether or not the cost of paint will be in budget

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,12 @@
 public class Main {
     public static void main (String[] args){
-
         String paintBrand = "Dulux";
         paintBrand = paintBrand.toUpperCase();
         String clientFirstName = "Paddington";
         clientFirstName = clientFirstName.toUpperCase();
         String clientLastname = "Brown";
         clientLastname = clientLastname.toUpperCase();
-
-
+        int paintBudget = 50;
         int priceOfPaintPerLitre = 5;
         int paintCoveragePerLitre = 10;
         int numberOfCoatings = 3;
@@ -21,25 +19,40 @@ public class Main {
         int wall2Length = (int) (1 + Math.random()*8);
         int wall2Area = wall2Height * wall2Length;
 
+        String ladderRequired = (wall1Height > 2||wall2Height > 2) ? ("A ladder is needed") : ("A ladder is not needed");
+        /*
+        double doorHeight = 2;
+        double doorLength = 0.5;
+        double doorArea = doorHeight * doorLength;
 
+        double windowHeight = 1 + Math.random()*3;
+        double windowLength = 1 + Math.random()*8;
+        double windowArea = windowHeight * windowLength;
+        */
         int totalWallsArea = wall1Area + wall2Area;
+        //double totalBlankSpace = doorArea + windowArea;
+        //double totalCoatingArea = (totalWallsArea - totalBlankSpace) * numberOfCoatings;
+        double totalCoatingArea = totalWallsArea * numberOfCoatings;
 
-        int totalCoatingArea = totalWallsArea * numberOfCoatings;
-
-        double paintBucketsNeeded = (double) totalCoatingArea / paintCoveragePerLitre;
+        double paintBucketsNeeded = totalCoatingArea / paintCoveragePerLitre;
         double wholePaintBucketsNeeded = Math.ceil(paintBucketsNeeded);
         double totalPriceOfPaint = wholePaintBucketsNeeded * priceOfPaintPerLitre;
 
         System.out.println("Paint Brand: " + paintBrand);
         System.out.println("Client Name: " + clientFirstName + " " + clientLastname);
+        System.out.println(ladderRequired);
         System.out.println("Area of Wall 1: " + wall1Area + " metres squared");
         System.out.println("Area of Wall 2: " + wall2Area + " metres squared");
         System.out.println("Total Area: " + totalWallsArea + " metres squared" );
-        System.out.println("Total area of coatings: " + totalCoatingArea);
+        //System.out.println("Total area of coatings: " + totalCoatingArea);
         System.out.println("Number of paint buckets needed: " + wholePaintBucketsNeeded);
         System.out.println("Total cost: " + totalPriceOfPaint);
+        System.out.println("Budget: " + paintBudget);
 
-
-
+        if(totalPriceOfPaint > paintBudget){
+            System.out.println("You do not have enough money");
+        } else {
+            System.out.println("You are within Budget");
+        }
     }
 }
