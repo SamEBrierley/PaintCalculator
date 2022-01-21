eb834ca64efe463a63879e754a7879ffef60539a
Sam Ellis Brierley <samellisbrierley@gmail.com>
2022-01-21 10:34:15 +0000

    Added the ability to remove areas of walls that will not be painted. Additionally, creates these areas randomly but have to be smaller than one of the walls.

--- src/Main.java
+++ src/Main.java
@@ -6,6 +6,7 @@ public class Main {
         clientFirstName = clientFirstName.toUpperCase();
         String clientLastname = "Brown";
         clientLastname = clientLastname.toUpperCase();
+
         int paintBudget = 50;
         int priceOfPaintPerLitre = 5;
         int paintCoveragePerLitre = 10;
@@ -27,32 +28,64 @@ public class Main {
         int wall2Area = wall2Height * wall2Length;
 
         String ladderRequired = (wall1Height > 2||wall2Height > 2) ? ("A ladder is needed") : ("A ladder is not needed");
-        /*
-        double doorHeight = 2;
-        double doorLength = 0.5;
+
+        double doorHeight = 0;
+        double doorLength = 0;
+
+        do{
+            doorHeight = (Math.random()*4);
+        } while (doorHeight >= wall1Height  || doorHeight >= wall2Height);
+
+        do{
+            doorLength = (Math.random()*2);
+        }while (doorLength >= wall1Length  || doorLength >= wall2Length);
+
         double doorArea = doorHeight * doorLength;
 
-        double windowHeight = 1 + Math.random()*3;
-        double windowLength = 1 + Math.random()*8;
+        double windowHeight = 0;
+        double windowLength = 0;
+
+        do{
+            windowHeight = (Math.random()*4);
+        } while (windowHeight >= wall1Height || windowHeight >= wall2Height);
+
+        do{
+            windowLength = (Math.random()*4);
+        }while(windowLength >= wall1Length || windowLength >= wall2Length);
+
+        System.out.println("");
+        System.out.println(windowHeight);
+        System.out.println(windowLength);
+
         double windowArea = windowHeight * windowLength;
-        */
+
         int totalWallsArea = wall1Area + wall2Area;
-        //double totalBlankSpace = doorArea + windowArea;
-        //double totalCoatingArea = (totalWallsArea - totalBlankSpace) * numberOfCoatings;
-        double totalCoatingArea = totalWallsArea * numberOfCoatings;
+        double totalBlankSpace = doorArea + windowArea;
+        double totalCoatingArea = (totalWallsArea - totalBlankSpace) * numberOfCoatings;
 
         double paintBucketsNeeded = totalCoatingArea / paintCoveragePerLitre;
         double wholePaintBucketsNeeded = Math.ceil(paintBucketsNeeded);
         double totalPriceOfPaint = wholePaintBucketsNeeded * priceOfPaintPerLitre;
 
-        System.out.println("Paint Brand: " + paintBrand);
+        System.out.println("---------Client-----------");
         System.out.println("Client Name: " + clientFirstName + " " + clientLastname);
-        System.out.println(ladderRequired);
+        System.out.println("");
+        System.out.println("---------Paint------------");
+        System.out.println("Paint Brand: " + paintBrand);
         System.out.println("Paint Colour: " + paintColour.toUpperCase());
+        System.out.println("");
+        System.out.println("---------Walls------------");
         System.out.println("Area of Wall 1: " + wall1Area + " metres squared");
         System.out.println("Area of Wall 2: " + wall2Area + " metres squared");
         System.out.println("Total Area: " + totalWallsArea + " metres squared" );
-        //System.out.println("Total area of coatings: " + totalCoatingArea);
+        System.out.println("");
+        System.out.println("------No Paint Area--------");
+        System.out.println("Area of Window: " + windowArea + " metres squared");
+        System.out.println("Area of Door: " + doorArea + " metres squared");
+        System.out.println("Total Void Area: " + totalBlankSpace + " metres squared" );
+        System.out.println("");
+        System.out.println("----------Totals-----------");
+        System.out.println("Total area of coatings: " + totalCoatingArea);
         System.out.println("Number of paint buckets needed: " + wholePaintBucketsNeeded);
         System.out.println("Total cost: " + totalPriceOfPaint);
         System.out.println("Budget: " + paintBudget);
@@ -62,5 +95,8 @@ public class Main {
         } else {
             System.out.println("You are within Budget");
         }
+        System.out.println("");
+        System.out.println("-----------Misc-------------");
+        System.out.println(ladderRequired);
     }
 }
